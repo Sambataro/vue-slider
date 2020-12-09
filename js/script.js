31a648a4089b1068dc4c304d650b157334db731b
fortunato <sambataro1990@gmail.com>
2020-12-09 16:05:30 +0100

    creato slide con immagini
    mancano i pallini

--- js/script.js
+++ js/script.js
@@ -3,6 +3,31 @@ var myObject = new Vue({
   el: '#app',
   data:
   {
-    
+    imageIndex: 0,
+    images: [
+      "https://nightcity.gg/wp-content/uploads/2020/06/Cyberpunk-2077-Character-Life-Paths-1024x602.jpg",
+      "https://static.wikia.nocookie.net/cyberpunk/images/4/46/Nomad_MV_Guide.png/revision/latest/scale-to-width-down/657?cb=20200626140242",
+      "https://static.wikia.nocookie.net/cyberpunk/images/7/7d/Corp_FV_Guide.JPG/revision/latest/scale-to-width-down/1000?cb=20200626031237",
+      "https://static.wikia.nocookie.net/cyberpunk/images/d/df/Streetkid_FV_Guide_2.jpg/revision/latest/scale-to-width-down/1000?cb=20200626031237"
+
+    ]
   },
-});
+  methods: {
+    nextImage: function() {
+      this.imageIndex++;
+
+      if (this.imageIndex == this.images.length) {
+        this.imageIndex = 0;
+      }
+    },
+
+    prevImage: function() {
+      this.imageIndex--;
+
+      if (this.imageIndex == -1) {
+        this.imageIndex = this.images.length -1;
+      }
+    }
+  }
+ }
+);
