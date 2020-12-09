c4c9545c64f9831159f77d2e5fa80217b9af1fe6
fortunato <sambataro1990@gmail.com>
2020-12-09 17:44:20 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -5,13 +5,20 @@ var myObject = new Vue({
   {
     imageIndex: 0,
     images: [
-      "https://nightcity.gg/wp-content/uploads/2020/06/Cyberpunk-2077-Character-Life-Paths-1024x602.jpg",
-      "https://static.wikia.nocookie.net/cyberpunk/images/4/46/Nomad_MV_Guide.png/revision/latest/scale-to-width-down/657?cb=20200626140242",
-      "https://static.wikia.nocookie.net/cyberpunk/images/7/7d/Corp_FV_Guide.JPG/revision/latest/scale-to-width-down/1000?cb=20200626031237",
-      "https://static.wikia.nocookie.net/cyberpunk/images/d/df/Streetkid_FV_Guide_2.jpg/revision/latest/scale-to-width-down/1000?cb=20200626031237"
+      "img/lifepaths.jpg",
+      "img/nomad.png",
+      "img/Streetkid.jpg",
+      "img/corp.jpg"
 
     ]
   },
+
+  created: function() {
+    setInterval(this.nextImage, 4000);
+
+  },
+
+
   methods: {
     nextImage: function() {
       this.imageIndex++;
@@ -20,7 +27,6 @@ var myObject = new Vue({
         this.imageIndex = 0;
       }
     },
-
     prevImage: function() {
       this.imageIndex--;
 
